ae2ba2d2a872b910b8dc3eb194b0025afb3ace72
17805055971 <1050086290@qq.com>
2022-01-14 23:38:19 +0800

    第二次提交

--- app/src/main/java/com/example/account_ms/activity/MainActivity.java
+++ app/src/main/java/com/example/account_ms/activity/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.account_ms.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.account_ms.R;
 
@@ -12,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Context mContext;
+        mContext = getApplicationContext();
+        Toast.makeText(this, "第一次提交", Toast.LENGTH_SHORT);
     }
 }
